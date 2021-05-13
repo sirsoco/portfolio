2a1bbebc45238657277b94f92e33d61731110481
sirsoco <66388516+sirsoco@users.noreply.github.com>
2021-05-12 23:59:58 -0500

    rendereded new title

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,53 +1,68 @@
-import logo from './logo.svg';
-import landingPage from './landingPage.js';
-import Container from '@material-ui/core/Container';
-import HeadShot from './HeadShot.jpeg'
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid'
-;
-import Typography from '@material-ui/core/Typography';
-import './App.css';
+import HeadShot from "./HeadShot.jpeg";
+import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { Container } from "@material-ui/core";
+
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  root: { 
     flexGrow: 1,
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(3),
+    margin: 'auto',
+    maxWidth: 1170,
   },
-  container: {
-    padding: theme.spacing()
+  banner: {
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
+  navigation: {
+    display: 'block',
+    float: 'right',
+    paddingTop: theme.spacing(2),
+    paddingLeft: theme.spacing(2)
   },
   bioText: {
-   justifyContent: 'center',
-   marginLeft: theme.spacing(60)
+    justifyContent: "center",
+    display: 'inline-block',
+    marginLeft: theme.spacing(60),
   },
-  HeadShot: {
-    margin: 'auto',
-    display: 'block',
+  headshot: {
+    margin: "auto",
+    display: "inline-block",
     height: 274,
-    width: 274
+    width: 274,
   },
-}))
+  intro: {
+    justifyContent: "center",
+    display: 'inline-block',
+    marginLeft: theme.spacing(60),
+  }
+}));
 
 function App() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-            <Grid className={classes.container} container>
-              <Grid item>
-                <img className={classes.HeadShot} src={HeadShot}/>
-              </Grid>
-            <Grid  className= {classes.bioText}item>
-              <Typography align='center'>
-                Solomon Obure
-              </Typography>
-              <Typography align='center'>
-                Full Stack Engineer
-              </Typography>
-              <Typography align='center'>
-                remote
-              </Typography>
-            </Grid>
-          
-            </Grid>
+
+      <Grid container maxWidth='false' container md={4} sm={6} xs={12} >
+        <Grid itemclassName={classes.brand}>
+          <Typography>
+            Solomon Obure
+          </Typography>
+        </Grid>
+        <Grid item md={4} sm={6} xs={12} className={classes.navigation}>
+          <ul>
+            <li>Work</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </Grid>
+      </Grid>
+     
     </div>
   );
 }
